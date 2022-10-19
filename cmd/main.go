bd90f1cbeaafa5b26e14dbeb511410da56c154d9
Justin <justin.100600@gmail.com>
2022-10-19 18:29:49 +0800

    feature: health check for auth/user service

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
 	"gitlab.com/cs302-2022/g2-team3/services/authentication/pkg/config"
 	"gitlab.com/cs302-2022/g2-team3/services/authentication/pkg/db"
 	"gitlab.com/cs302-2022/g2-team3/services/authentication/pkg/pb/auth"
+	"gitlab.com/cs302-2022/g2-team3/services/authentication/pkg/pb/health"
 	"gitlab.com/cs302-2022/g2-team3/services/authentication/pkg/pb/users"
 	"gitlab.com/cs302-2022/g2-team3/services/authentication/pkg/services"
 	"gitlab.com/cs302-2022/g2-team3/services/authentication/pkg/utils"
@@ -16,7 +17,6 @@ import (
 
 func main() {
 	c, err := config.LoadConfig()
-
 	if err != nil {
 		log.Fatalln("Failed at config", err)
 	}
@@ -30,7 +30,6 @@ func main() {
 	}
 
 	lis, err := net.Listen("tcp", c.Port)
-
 	if err != nil {
 		log.Fatalln("Failed to listen:", err)
 	}
@@ -46,10 +45,13 @@ func main() {
 		H: h,
 	}
 
+	healthServer := services.HealthServer{}
+
 	grpcServer := grpc.NewServer()
 
 	auth_proto.RegisterAuthServiceServer(grpcServer, &authServer)
 	users_proto.RegisterUserServiceServer(grpcServer, &userServer)
+	health_proto.RegisterHealthServer(grpcServer, &healthServer)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalln("Failed to serve:", err)
