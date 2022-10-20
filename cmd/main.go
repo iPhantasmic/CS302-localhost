8e34b6748199de42efa420ab56d86033eb5604a6
Justin <justin.100600@gmail.com>
2022-10-21 04:38:23 +0800

    feature: initialise boilerplate

---
+++ cmd/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"gitlab.com/cs302-2022/g2-team3/services/listings/pkg/config"
+	"gitlab.com/cs302-2022/g2-team3/services/listings/pkg/db"
+	"gitlab.com/cs302-2022/g2-team3/services/listings/pkg/pb/health"
+	"gitlab.com/cs302-2022/g2-team3/services/listings/pkg/pb/listings"
+	"gitlab.com/cs302-2022/g2-team3/services/listings/pkg/services"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	c, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalln("Failed to load configs", err)
+	}
+
+	h := db.Init(c.DBUrl, c.Schema)
+
+	lis, err := net.Listen("tcp", c.Port)
+	if err != nil {
+		log.Fatalln("Failed to listen:", err)
+	}
+
+	fmt.Println("Listings Service listening on", c.Port)
+
+	listingServer := services.ListingServer{
+		H: h,
+	}
+
+	healthServer := services.HealthServer{}
+
+	grpcServer := grpc.NewServer()
+
+	listings_proto.RegisterListingServiceServer(grpcServer, &listingServer)
+	health_proto.RegisterHealthServer(grpcServer, &healthServer)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalln("Failed to serve:", err)
+	}
+}
