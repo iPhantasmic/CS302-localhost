edfccb9dc338760deb67bbace91e022de4bd37f9
Justin <justin.100600@gmail.com>
2022-10-23 23:10:21 +0800

    fix: camelcase for graphql schema

--- dist/resolvers/getUser.js
+++ dist/resolvers/getUser.js
@@ -2,11 +2,9 @@ import UserClient from '../services/User/UserClient';
 const client = UserClient();
 export default (root, params) => {
     return new Promise((resolve, reject) => {
-        // console.log(params.data)
-        // console.log(params.data.user_id)
-        // console.log(params.user_id)
+        console.log(params.data);
         // client.GetUser(params.data, function(err: any, response: any) {
-        client.GetUser({ "user_id": "51ff89ea-0f82-45b8-a4e8-c39c05bf7dcf" }, function (err, response) {
+        client.GetUser(params.data, function (err, response) {
             if (err) {
                 return reject(err);
             }
