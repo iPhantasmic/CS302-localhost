c810a27a204f381698060e0c524fbe478fd34c1f
Justin <justin.100600@gmail.com>
2022-10-26 21:23:39 +0800

    chore: update bookings proto

--- bookings/bookings_pb2.py
+++ bookings/bookings_pb2.py
@@ -5,30 +5,30 @@ from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import symbol_database as _symbol_database
 _sym_db = _symbol_database.Default()
 from google.protobuf import timestamp_pb2 as google_dot_protobuf_dot_timestamp__pb2
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0ebookings.proto\x12\x08bookings\x1a\x1fgoogle/protobuf/timestamp.proto"\x85\x01\n\x07Booking\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x0f\n\x07user_id\x18\x02 \x01(\x05\x12\x12\n\nlisting_id\x18\x03 \x01(\x05\x12\x0f\n\x07host_id\x18\x04 \x01(\x05\x12\x12\n\nstart_date\x18\x05 \x01(\t\x12\x10\n\x08end_date\x18\x06 \x01(\t\x12\x12\n\npayment_id\x18\x07 \x01(\x05"\x90\x01\n\x1bGetAvailableListingsRequest\x12\x13\n\x0blisting_ids\x18\x01 \x03(\x05\x12.\n\nstart_date\x18\x02 \x01(\x0b2\x1a.google.protobuf.Timestamp\x12,\n\x08end_date\x18\x03 \x01(\x0b2\x1a.google.protobuf.Timestamp"3\n\x1cGetAvailableListingsResponse\x12\x13\n\x0blisting_ids\x18\x01 \x03(\t"\x86\x01\n\x14CreateBookingRequest\x12\x0f\n\x07user_id\x18\x01 \x01(\x05\x12\x12\n\nlisting_id\x18\x02 \x01(\x05\x12\x0f\n\x07host_id\x18\x03 \x01(\x05\x12\x12\n\nstart_date\x18\x04 \x01(\t\x12\x10\n\x08end_date\x18\x05 \x01(\t\x12\x12\n\npayment_id\x18\x06 \x01(\x05">\n\x17GetBookingArrayResponse\x12#\n\x08bookings\x18\x01 \x03(\x0b2\x11.bookings.Booking"-\n\x17BookingByListingRequest\x12\x12\n\nlisting_id\x18\x01 \x01(\x05"\'\n\x14BookingByUserRequest\x12\x0f\n\x07user_id\x18\x01 \x01(\x05"(\n\x12BookingByIdRequest\x12\x12\n\nbooking_id\x18\x01 \x01(\x05"\x92\x01\n\x14UpdateBookingRequest\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x0f\n\x07user_id\x18\x02 \x01(\x05\x12\x12\n\nlisting_id\x18\x03 \x01(\x05\x12\x0f\n\x07host_id\x18\x04 \x01(\x05\x12\x12\n\nstart_date\x18\x05 \x01(\t\x12\x10\n\x08end_date\x18\x06 \x01(\t\x12\x12\n\npayment_id\x18\x07 \x01(\x05"\'\n\rReturnMessage\x12\x16\n\x0ereturn_message\x18\x01 \x01(\t2\xc3\x05\n\x0eBookingService\x12D\n\rCreateBooking\x12\x1e.bookings.CreateBookingRequest\x1a\x11.bookings.Booking"\x00\x12W\n\x10GetBookingByUser\x12\x1e.bookings.BookingByUserRequest\x1a!.bookings.GetBookingArrayResponse"\x00\x12]\n\x13GetBookingByListing\x12!.bookings.BookingByListingRequest\x1a!.bookings.GetBookingArrayResponse"\x00\x12N\n\x11UpdateBookingById\x12\x1e.bookings.UpdateBookingRequest\x1a\x17.bookings.ReturnMessage"\x00\x12R\n\x15DeleteBookingByUserId\x12\x1e.bookings.BookingByUserRequest\x1a\x17.bookings.ReturnMessage"\x00\x12X\n\x18DeleteBookingByListingId\x12!.bookings.BookingByListingRequest\x1a\x17.bookings.ReturnMessage"\x00\x12g\n\x14GetAvailableListings\x12%.bookings.GetAvailableListingsRequest\x1a&.bookings.GetAvailableListingsResponse"\x00\x12L\n\x11DeleteBookingById\x12\x1c.bookings.BookingByIdRequest\x1a\x17.bookings.ReturnMessage"\x00b\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0ebookings.proto\x12\x08bookings\x1a\x1fgoogle/protobuf/timestamp.proto"\xbd\x01\n\x07Booking\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0f\n\x07user_id\x18\x02 \x01(\t\x12\x12\n\nlisting_id\x18\x03 \x01(\t\x12\x0f\n\x07host_id\x18\x04 \x01(\t\x12.\n\nstart_date\x18\x05 \x01(\x0b2\x1a.google.protobuf.Timestamp\x12,\n\x08end_date\x18\x06 \x01(\x0b2\x1a.google.protobuf.Timestamp\x12\x12\n\npayment_id\x18\x07 \x01(\t"\x90\x01\n\x1bGetAvailableListingsRequest\x12\x13\n\x0blisting_ids\x18\x01 \x03(\t\x12.\n\nstart_date\x18\x02 \x01(\x0b2\x1a.google.protobuf.Timestamp\x12,\n\x08end_date\x18\x03 \x01(\x0b2\x1a.google.protobuf.Timestamp"3\n\x1cGetAvailableListingsResponse\x12\x13\n\x0blisting_ids\x18\x01 \x03(\t"\xbe\x01\n\x14CreateBookingRequest\x12\x0f\n\x07user_id\x18\x01 \x01(\t\x12\x12\n\nlisting_id\x18\x02 \x01(\t\x12\x0f\n\x07host_id\x18\x03 \x01(\t\x12.\n\nstart_date\x18\x04 \x01(\x0b2\x1a.google.protobuf.Timestamp\x12,\n\x08end_date\x18\x05 \x01(\x0b2\x1a.google.protobuf.Timestamp\x12\x12\n\npayment_id\x18\x06 \x01(\t">\n\x17GetBookingArrayResponse\x12#\n\x08bookings\x18\x01 \x03(\x0b2\x11.bookings.Booking"-\n\x17BookingByListingRequest\x12\x12\n\nlisting_id\x18\x01 \x01(\t"\'\n\x14BookingByUserRequest\x12\x0f\n\x07user_id\x18\x01 \x01(\t"(\n\x12BookingByIdRequest\x12\x12\n\nbooking_id\x18\x01 \x01(\t"\xca\x01\n\x14UpdateBookingRequest\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0f\n\x07user_id\x18\x02 \x01(\t\x12\x12\n\nlisting_id\x18\x03 \x01(\t\x12\x0f\n\x07host_id\x18\x04 \x01(\t\x12.\n\nstart_date\x18\x05 \x01(\x0b2\x1a.google.protobuf.Timestamp\x12,\n\x08end_date\x18\x06 \x01(\x0b2\x1a.google.protobuf.Timestamp\x12\x12\n\npayment_id\x18\x07 \x01(\t"\'\n\rReturnMessage\x12\x16\n\x0ereturn_message\x18\x01 \x01(\t2\xc3\x05\n\x0eBookingService\x12D\n\rCreateBooking\x12\x1e.bookings.CreateBookingRequest\x1a\x11.bookings.Booking"\x00\x12W\n\x10GetBookingByUser\x12\x1e.bookings.BookingByUserRequest\x1a!.bookings.GetBookingArrayResponse"\x00\x12]\n\x13GetBookingByListing\x12!.bookings.BookingByListingRequest\x1a!.bookings.GetBookingArrayResponse"\x00\x12N\n\x11UpdateBookingById\x12\x1e.bookings.UpdateBookingRequest\x1a\x17.bookings.ReturnMessage"\x00\x12R\n\x15DeleteBookingByUserId\x12\x1e.bookings.BookingByUserRequest\x1a\x17.bookings.ReturnMessage"\x00\x12X\n\x18DeleteBookingByListingId\x12!.bookings.BookingByListingRequest\x1a\x17.bookings.ReturnMessage"\x00\x12L\n\x11DeleteBookingById\x12\x1c.bookings.BookingByIdRequest\x1a\x17.bookings.ReturnMessage"\x00\x12g\n\x14GetAvailableListings\x12%.bookings.GetAvailableListingsRequest\x1a&.bookings.GetAvailableListingsResponse"\x00b\x06proto3')
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'bookings_pb2', globals())
 if _descriptor._USE_C_DESCRIPTORS == False:
     DESCRIPTOR._options = None
     _BOOKING._serialized_start = 62
-    _BOOKING._serialized_end = 195
-    _GETAVAILABLELISTINGSREQUEST._serialized_start = 198
-    _GETAVAILABLELISTINGSREQUEST._serialized_end = 342
-    _GETAVAILABLELISTINGSRESPONSE._serialized_start = 344
-    _GETAVAILABLELISTINGSRESPONSE._serialized_end = 395
-    _CREATEBOOKINGREQUEST._serialized_start = 398
-    _CREATEBOOKINGREQUEST._serialized_end = 532
-    _GETBOOKINGARRAYRESPONSE._serialized_start = 534
-    _GETBOOKINGARRAYRESPONSE._serialized_end = 596
-    _BOOKINGBYLISTINGREQUEST._serialized_start = 598
-    _BOOKINGBYLISTINGREQUEST._serialized_end = 643
-    _BOOKINGBYUSERREQUEST._serialized_start = 645
-    _BOOKINGBYUSERREQUEST._serialized_end = 684
-    _BOOKINGBYIDREQUEST._serialized_start = 686
-    _BOOKINGBYIDREQUEST._serialized_end = 726
-    _UPDATEBOOKINGREQUEST._serialized_start = 729
-    _UPDATEBOOKINGREQUEST._serialized_end = 875
-    _RETURNMESSAGE._serialized_start = 877
-    _RETURNMESSAGE._serialized_end = 916
-    _BOOKINGSERVICE._serialized_start = 919
-    _BOOKINGSERVICE._serialized_end = 1626
+    _BOOKING._serialized_end = 251
+    _GETAVAILABLELISTINGSREQUEST._serialized_start = 254
+    _GETAVAILABLELISTINGSREQUEST._serialized_end = 398
+    _GETAVAILABLELISTINGSRESPONSE._serialized_start = 400
+    _GETAVAILABLELISTINGSRESPONSE._serialized_end = 451
+    _CREATEBOOKINGREQUEST._serialized_start = 454
+    _CREATEBOOKINGREQUEST._serialized_end = 644
+    _GETBOOKINGARRAYRESPONSE._serialized_start = 646
+    _GETBOOKINGARRAYRESPONSE._serialized_end = 708
+    _BOOKINGBYLISTINGREQUEST._serialized_start = 710
+    _BOOKINGBYLISTINGREQUEST._serialized_end = 755
+    _BOOKINGBYUSERREQUEST._serialized_start = 757
+    _BOOKINGBYUSERREQUEST._serialized_end = 796
+    _BOOKINGBYIDREQUEST._serialized_start = 798
+    _BOOKINGBYIDREQUEST._serialized_end = 838
+    _UPDATEBOOKINGREQUEST._serialized_start = 841
+    _UPDATEBOOKINGREQUEST._serialized_end = 1043
+    _RETURNMESSAGE._serialized_start = 1045
+    _RETURNMESSAGE._serialized_end = 1084
+    _BOOKINGSERVICE._serialized_start = 1087
+    _BOOKINGSERVICE._serialized_end = 1794
--- bookings/bookings_pb2_grpc.py
+++ bookings/bookings_pb2_grpc.py
@@ -17,8 +17,8 @@ class BookingServiceStub(object):
         self.UpdateBookingById = channel.unary_unary('/bookings.BookingService/UpdateBookingById', request_serializer=bookings__pb2.UpdateBookingRequest.SerializeToString, response_deserializer=bookings__pb2.ReturnMessage.FromString)
         self.DeleteBookingByUserId = channel.unary_unary('/bookings.BookingService/DeleteBookingByUserId', request_serializer=bookings__pb2.BookingByUserRequest.SerializeToString, response_deserializer=bookings__pb2.ReturnMessage.FromString)
         self.DeleteBookingByListingId = channel.unary_unary('/bookings.BookingService/DeleteBookingByListingId', request_serializer=bookings__pb2.BookingByListingRequest.SerializeToString, response_deserializer=bookings__pb2.ReturnMessage.FromString)
-        self.GetAvailableListings = channel.unary_unary('/bookings.BookingService/GetAvailableListings', request_serializer=bookings__pb2.GetAvailableListingsRequest.SerializeToString, response_deserializer=bookings__pb2.GetAvailableListingsResponse.FromString)
         self.DeleteBookingById = channel.unary_unary('/bookings.BookingService/DeleteBookingById', request_serializer=bookings__pb2.BookingByIdRequest.SerializeToString, response_deserializer=bookings__pb2.ReturnMessage.FromString)
+        self.GetAvailableListings = channel.unary_unary('/bookings.BookingService/GetAvailableListings', request_serializer=bookings__pb2.GetAvailableListingsRequest.SerializeToString, response_deserializer=bookings__pb2.GetAvailableListingsResponse.FromString)
 
 class BookingServiceServicer(object):
     """Missing associated documentation comment in .proto file."""
@@ -59,20 +59,20 @@ class BookingServiceServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
-    def GetAvailableListings(self, request, context):
-        """Missing associated documentation comment in .proto file."""
-        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
-        context.set_details('Method not implemented!')
-        raise NotImplementedError('Method not implemented!')
-
     def DeleteBookingById(self, request, context):
         """Missing associated documentation comment in .proto file."""
         context.set_code(grpc.StatusCode.UNIMPLEMENTED)
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def GetAvailableListings(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 def add_BookingServiceServicer_to_server(servicer, server):
-    rpc_method_handlers = {'CreateBooking': grpc.unary_unary_rpc_method_handler(servicer.CreateBooking, request_deserializer=bookings__pb2.CreateBookingRequest.FromString, response_serializer=bookings__pb2.Booking.SerializeToString), 'GetBookingByUser': grpc.unary_unary_rpc_method_handler(servicer.GetBookingByUser, request_deserializer=bookings__pb2.BookingByUserRequest.FromString, response_serializer=bookings__pb2.GetBookingArrayResponse.SerializeToString), 'GetBookingByListing': grpc.unary_unary_rpc_method_handler(servicer.GetBookingByListing, request_deserializer=bookings__pb2.BookingByListingRequest.FromString, response_serializer=bookings__pb2.GetBookingArrayResponse.SerializeToString), 'UpdateBookingById': grpc.unary_unary_rpc_method_handler(servicer.UpdateBookingById, request_deserializer=bookings__pb2.UpdateBookingRequest.FromString, response_serializer=bookings__pb2.ReturnMessage.SerializeToString), 'DeleteBookingByUserId': grpc.unary_unary_rpc_method_handler(servicer.DeleteBookingByUserId, request_deserializer=bookings__pb2.BookingByUserRequest.FromString, response_serializer=bookings__pb2.ReturnMessage.SerializeToString), 'DeleteBookingByListingId': grpc.unary_unary_rpc_method_handler(servicer.DeleteBookingByListingId, request_deserializer=bookings__pb2.BookingByListingRequest.FromString, response_serializer=bookings__pb2.ReturnMessage.SerializeToString), 'GetAvailableListings': grpc.unary_unary_rpc_method_handler(servicer.GetAvailableListings, request_deserializer=bookings__pb2.GetAvailableListingsRequest.FromString, response_serializer=bookings__pb2.GetAvailableListingsResponse.SerializeToString), 'DeleteBookingById': grpc.unary_unary_rpc_method_handler(servicer.DeleteBookingById, request_deserializer=bookings__pb2.BookingByIdRequest.FromString, response_serializer=bookings__pb2.ReturnMessage.SerializeToString)}
+    rpc_method_handlers = {'CreateBooking': grpc.unary_unary_rpc_method_handler(servicer.CreateBooking, request_deserializer=bookings__pb2.CreateBookingRequest.FromString, response_serializer=bookings__pb2.Booking.SerializeToString), 'GetBookingByUser': grpc.unary_unary_rpc_method_handler(servicer.GetBookingByUser, request_deserializer=bookings__pb2.BookingByUserRequest.FromString, response_serializer=bookings__pb2.GetBookingArrayResponse.SerializeToString), 'GetBookingByListing': grpc.unary_unary_rpc_method_handler(servicer.GetBookingByListing, request_deserializer=bookings__pb2.BookingByListingRequest.FromString, response_serializer=bookings__pb2.GetBookingArrayResponse.SerializeToString), 'UpdateBookingById': grpc.unary_unary_rpc_method_handler(servicer.UpdateBookingById, request_deserializer=bookings__pb2.UpdateBookingRequest.FromString, response_serializer=bookings__pb2.ReturnMessage.SerializeToString), 'DeleteBookingByUserId': grpc.unary_unary_rpc_method_handler(servicer.DeleteBookingByUserId, request_deserializer=bookings__pb2.BookingByUserRequest.FromString, response_serializer=bookings__pb2.ReturnMessage.SerializeToString), 'DeleteBookingByListingId': grpc.unary_unary_rpc_method_handler(servicer.DeleteBookingByListingId, request_deserializer=bookings__pb2.BookingByListingRequest.FromString, response_serializer=bookings__pb2.ReturnMessage.SerializeToString), 'DeleteBookingById': grpc.unary_unary_rpc_method_handler(servicer.DeleteBookingById, request_deserializer=bookings__pb2.BookingByIdRequest.FromString, response_serializer=bookings__pb2.ReturnMessage.SerializeToString), 'GetAvailableListings': grpc.unary_unary_rpc_method_handler(servicer.GetAvailableListings, request_deserializer=bookings__pb2.GetAvailableListingsRequest.FromString, response_serializer=bookings__pb2.GetAvailableListingsResponse.SerializeToString)}
     generic_handler = grpc.method_handlers_generic_handler('bookings.BookingService', rpc_method_handlers)
     server.add_generic_rpc_handlers((generic_handler,))
 
@@ -104,9 +104,9 @@ class BookingService(object):
         return grpc.experimental.unary_unary(request, target, '/bookings.BookingService/DeleteBookingByListingId', bookings__pb2.BookingByListingRequest.SerializeToString, bookings__pb2.ReturnMessage.FromString, options, channel_credentials, insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
 
     @staticmethod
-    def GetAvailableListings(request, target, options=(), channel_credentials=None, call_credentials=None, insecure=False, compression=None, wait_for_ready=None, timeout=None, metadata=None):
-        return grpc.experimental.unary_unary(request, target, '/bookings.BookingService/GetAvailableListings', bookings__pb2.GetAvailableListingsRequest.SerializeToString, bookings__pb2.GetAvailableListingsResponse.FromString, options, channel_credentials, insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
-
-    @staticmethod
     def DeleteBookingById(request, target, options=(), channel_credentials=None, call_credentials=None, insecure=False, compression=None, wait_for_ready=None, timeout=None, metadata=None):
         return grpc.experimental.unary_unary(request, target, '/bookings.BookingService/DeleteBookingById', bookings__pb2.BookingByIdRequest.SerializeToString, bookings__pb2.ReturnMessage.FromString, options, channel_credentials, insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def GetAvailableListings(request, target, options=(), channel_credentials=None, call_credentials=None, insecure=False, compression=None, wait_for_ready=None, timeout=None, metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/bookings.BookingService/GetAvailableListings', bookings__pb2.GetAvailableListingsRequest.SerializeToString, bookings__pb2.GetAvailableListingsResponse.FromString, options, channel_credentials, insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
