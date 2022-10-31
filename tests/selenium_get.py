d1759ad41fc98c5295e0a014d62590da9dc5366d
ONG CHI KIAT, NICHOLAS _ <nicholasong.2020@smu.edu.sg>
2022-10-31 21:05:34 +0800

    feat: selenium test

---
+++ tests/selenium_get.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+chrome_driver = webdriver.Remote("http://selenium:4444/wd/hub", desired_capabilities=DesiredCapabilities.CHROME)
+chrome_driver.get('https://cs302-ui.vercel.app/')
+
+title = "Localhost - Rent safely1"
+assert title == chrome_driver.title
+chrome_driver.close()
+chrome_driver.quit()
--- tests/test_get.py
+++ tests/test_get.py
@@ -1,7 +1,28 @@
 import pytest
 import requests
+from time import sleep
+
+# from selenium import webdriver
+# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 @pytest.mark.dependency()
 def test_function():
     x = requests.get("https://cs302-ui.vercel.app/")
     assert x.status_code == 200
+
+@pytest.mark.dependency()
+def test_title_app(selenium):
+
+    sleep(2)
+    # chrome_driver = webdriver.Remote("http://localhost:4444/wd/hub", desired_capabilities=DesiredCapabilities.CHROME)
+        
+    # chrome_driver.get('https://cs302-ui.vercel.app/')
+    selenium.get('https://cs302-ui.vercel.app/')
+    sleep(2)
+    # chrome_driver.maximize_window()
+    # chrome_driver.save_screenshot("screenshot.png")
+    title = "Localhost - Rent safely"
+    assert title == selenium.title
+
+# if __name__ == "__main__":
+#     test_title_app()
