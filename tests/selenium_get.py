d2e25e6735a48987d956c8dfe758be9e33007d50
ONG CHI KIAT, NICHOLAS _ <nicholasong.2020@smu.edu.sg>
2022-10-31 21:20:17 +0800

    chore: pass test

--- tests/selenium_get.py
+++ tests/selenium_get.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 chrome_driver = webdriver.Remote("http://selenium:4444/wd/hub", desired_capabilities=DesiredCapabilities.CHROME)
 chrome_driver.get('https://cs302-ui.vercel.app/')
 
-title = "Localhost - Rent safely1"
+title = "Localhost - Rent safely"
 assert title == chrome_driver.title
 chrome_driver.close()
 chrome_driver.quit()
