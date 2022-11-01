1cb13417368e6fda3e51e420806999f0fb4b25ac
ONG CHI KIAT, NICHOLAS _ <nicholasong.2020@smu.edu.sg>
2022-11-01 16:24:07 +0800

    feat: make bookings

--- src/resolvers/makeBooking.ts
+++ src/resolvers/makeBooking.ts
@@ -1,4 +1,3 @@
-
 import BookingClient from '../services/booking/BookingClient'
 import SNSPublisher from '../services/email/booking_confirmed'
 
@@ -16,39 +15,51 @@ export default (root: any, params: any) => {
         //     "start_date": "2022-04-22 10:34:23",
         //     "user_id": 1552476736
         // }
-        client.CreateBooking(
-            params.data,
-            function (err: any, response: any) {
-                if (err) {
-                    console.log(params.data)
-                    return reject(err)
-                }
-                const booking_id = response["id"]
-                console.log(booking_id)
-                client.DeleteBookingById(booking_id)
-
-                //Stripe call here:
-                //  - Rollback by deleting transaction ID
-                const test_data = {
-                    data:{
-                        receiverEmail:"nicholasong.2020@smu.edu.sg",
-                        propertyName: 'Seminyak Kuta Utara',
-                        receiptId: 'RC4E4KBBHD',
-                        paymentDate: 'October 17, 2022',
-                        paymentTime: '11:00:21 PM GMT+8',
-                        bookingStartDate: 'Thu, Dec 8, 2022',
-                        bookingEndDate: 'Tue, Dec 13, 2022',
-                        unitPrice: 125.1,
-                        numNights: 5,
-                        subTotal: 625.5,
-                        serviceFee: 95.5,
-                        totalPrice: 721.0,
-                    }
-                }
-
-                email_client.publish_message_booking_confirmed(test_data)
-                resolve(response)
+        client.CreateBooking(params.data, function (err: any, response: any) {
+            if (err) {
+                return reject(err)
             }
-        )
+
+            const bookingId = response['id']
+
+            const rollback_data = {
+                bookingId: bookingId,
+            }
+
+            try{
+                // Stripe Code
+            }catch{
+                //Rollback for booking in event payment fails
+                client.DeleteBookingById(
+                    rollback_data,
+                    function (err: any, response: any) {
+                        if (err) {
+                            return reject(err)
+                        }
+                    }
+                )
+            }
+
+            //Prepare message to send to SNS
+            //TODO: retrieve listing details from db
+            const test_data = {
+                data: {
+                    receiverEmail: 'nicholasong.2020@smu.edu.sg',
+                    propertyName: 'Seminyak Kuta Utara',
+                    receiptId: 'RC4E4KBBHD',
+                    paymentDate: 'October 17, 2022',
+                    paymentTime: '11:00:21 PM GMT+8',
+                    bookingStartDate: 'Thu, Dec 8, 2022',
+                    bookingEndDate: 'Tue, Dec 13, 2022',
+                    unitPrice: 125.1,
+                    numNights: 5,
+                    subTotal: 625.5,
+                    serviceFee: 95.5,
+                    totalPrice: 721.0,
+                },
+            }
+
+            resolve(response)
+        })
     })
 }
