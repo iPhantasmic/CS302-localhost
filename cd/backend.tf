cd8c174b3d2284f218aac2b113f81f32c8ad0a6a
Justin <justin.100600@gmail.com>
2022-11-01 21:49:51 +0800

    fix: upload lock file

--- cd/backend.tf
+++ cd/backend.tf
@@ -1,4 +1,9 @@
 terraform {
   backend "http" {
+    address = "https://gitlab.com/api/v4/projects/40084278/terraform/state/terraform.tfstate"
+    lock_address = "https://gitlab.com/api/v4/projects/40084278/terraform/state/terraform.tfstate/lock"
+    lock_method = "POST"
+    unlock_address = "https://gitlab.com/api/v4/projects/40084278/terraform/state/terraform.tfstate/lock"
+    unlock_method = "DELETE"
   }
 }
