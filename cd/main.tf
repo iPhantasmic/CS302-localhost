594714b7e92f9883906d5ceb8b2f11f1f343a8c9
Justin <justin.100600@gmail.com>
2022-11-03 01:12:01 +0800

    fix: convert to ec2 for static ip

--- cd/main.tf
+++ cd/main.tf
@@ -28,17 +28,13 @@ variable "JWT_SECRET_KEY" {
 
 resource "aws_ecs_task_definition" "auth-service" {
   family = "auth-service"
-  network_mode = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  memory = "1024"
-  cpu = "512"
   execution_role_arn       = "arn:aws:iam::631945473733:role/ecsTaskExecutionRole"
   container_definitions = jsonencode([
     {
       name = "auth-service"
       image = "631945473733.dkr.ecr.ap-southeast-1.amazonaws.com/authentication:latest"
       cpu = 512
-      memory = 1024
+      memory = 512
       essential = true
       portMappings = [{
         containerPort = 50051
@@ -70,10 +66,5 @@ resource "aws_ecs_service" "auth-service" {
   name = "auth-service"
   cluster = "arn:aws:ecs:ap-southeast-1:631945473733:cluster/cs302"
   task_definition = aws_ecs_task_definition.auth-service.arn
-  launch_type = "FARGATE"
-  network_configuration {
-    subnets = ["subnet-09d7fd774d9d85fc9"]
-    assign_public_ip = true
-  }
   desired_count = 1
 }
