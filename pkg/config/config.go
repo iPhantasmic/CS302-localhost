d0e0f278a6fa72bade32aab98b8955fd356958d7
Justin <justin.100600@gmail.com>
2022-11-03 18:03:46 +0800

    feature: pre-commit for golang

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -25,14 +25,14 @@ func LoadConfig() (config Config, err error) {
 
 	if err != nil {
 		// production use
-		err = viper.BindEnv("PORT")
-		err = viper.BindEnv("DB_USER")
-		err = viper.BindEnv("DB_PASSWORD")
-		err = viper.BindEnv("DB_HOST")
-		err = viper.BindEnv("DB_PORT")
-		err = viper.BindEnv("DB_SCHEMA")
-		err = viper.BindEnv("JWT_SECRET_KEY")
-		err = viper.Unmarshal(&config)
+		_ = viper.BindEnv("PORT")
+		_ = viper.BindEnv("DB_USER")
+		_ = viper.BindEnv("DB_PASSWORD")
+		_ = viper.BindEnv("DB_HOST")
+		_ = viper.BindEnv("DB_PORT")
+		_ = viper.BindEnv("DB_SCHEMA")
+		_ = viper.BindEnv("JWT_SECRET_KEY")
+		_ = viper.Unmarshal(&config)
 		return
 	}
 
