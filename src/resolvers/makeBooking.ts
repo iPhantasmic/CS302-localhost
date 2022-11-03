9cb4af0eb9951e68f48620b47b86de2f4a734935
ONG CHI KIAT, NICHOLAS _ <nicholasong.2020@smu.edu.sg>
2022-11-03 11:43:49 +0800

    feat: change logic for make Booking

--- src/resolvers/makeBooking.ts
+++ src/resolvers/makeBooking.ts
@@ -26,9 +26,31 @@ export default (root: any, params: any) => {
                 bookingId: bookingId,
             }
 
-            try{
+            try {
                 // Stripe Code
-            }catch{
+
+                //Prepare message to send to SNS
+                //TODO: retrieve listing details from db
+                const test_data = {
+                    data: {
+                        receiverEmail: 'nicholasong.2020@smu.edu.sg',
+                        propertyName: 'Seminyak Kuta Utara',
+                        receiptId: 'RC4E4KBBHD',
+                        paymentDate: 'October 17, 2022',
+                        paymentTime: '11:00:21 PM GMT+8',
+                        bookingStartDate: 'Thu, Dec 8, 2022',
+                        bookingEndDate: 'Tue, Dec 13, 2022',
+                        unitPrice: 125.1,
+                        numNights: 5,
+                        subTotal: 625.5,
+                        serviceFee: 95.5,
+                        totalPrice: 721.0,
+                    },
+                }
+                email_client.publish_message_booking_confirmed(test_data)
+
+                resolve(response)
+            } catch {
                 //Rollback for booking in event payment fails
                 client.DeleteBookingById(
                     rollback_data,
@@ -39,30 +61,6 @@ export default (root: any, params: any) => {
                     }
                 )
             }
-
-            //Prepare message to send to SNS
-            //TODO: retrieve listing details from db
-            const test_data = {
-                data: {
-                    receiverEmail: 'nicholasong.2020@smu.edu.sg',
-                    propertyName: 'Seminyak Kuta Utara',
-                    receiptId: 'RC4E4KBBHD',
-                    paymentDate: 'October 17, 2022',
-                    paymentTime: '11:00:21 PM GMT+8',
-                    bookingStartDate: 'Thu, Dec 8, 2022',
-                    bookingEndDate: 'Tue, Dec 13, 2022',
-                    unitPrice: 125.1,
-                    numNights: 5,
-                    subTotal: 625.5,
-                    serviceFee: 95.5,
-                    totalPrice: 721.0,
-                },
-            }
-            email_client.publish_message_booking_confirmed(
-                test_data
-            )
-
-            resolve(response)
         })
     })
 }
