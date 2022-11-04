52741df84552a61a7861464688532be5f270e5fc
ONG CHI KIAT, NICHOLAS _ <nicholasong.2020@smu.edu.sg>
2022-11-04 16:47:48 +0800

    feat: new options arg

--- tests/selenium_get.py
+++ tests/selenium_get.py
@@ -1,9 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-chrome_driver = webdriver.Remote("http://selenium:4444/wd/hub", desired_capabilities=DesiredCapabilities.CHROME)
+from selenium.webdriver import ChromeOptions
+
+
+
+options = ChromeOptions()
+chrome_driver = webdriver.Remote("http://selenium:4444/wd/hub", options=options)
 chrome_driver.get('https://cs302-ui.vercel.app/')
 
 title = "Localhost - Rent safely"
 assert title == chrome_driver.title
+print("Passed!")
 chrome_driver.close()
 chrome_driver.quit()
--- tests/selenium_test.py
+++ tests/selenium_test.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-chrome_driver = webdriver.Remote("http://selenium:4444/wd/hub", desired_capabilities=DesiredCapabilities.CHROME)
+
+
+options = ChromeOptions()
+chrome_driver = webdriver.Remote("http://selenium:4444/wd/hub", options=options)
 chrome_driver.get('https://cs302-ui.vercel.app/')
 
 title = "Localhost - Rent safely1212"
 assert title != chrome_driver.title
+print("Passed!")
 chrome_driver.close()
 chrome_driver.quit()
