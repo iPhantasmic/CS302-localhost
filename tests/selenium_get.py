dd733f909c038b25280c622c5023d3e13795da5b
ONG CHI KIAT, NICHOLAS _ <nicholasong.2020@smu.edu.sg>
2022-11-04 22:01:54 +0800

    fix:binary location

--- tests/selenium_get.py
+++ tests/selenium_get.py
@@ -12,7 +12,7 @@ options.add_argument("--disable-extensions");
 options.add_argument("--disable-gpu");
 options.add_argument("--disable-dev-shm-usage");
 options.add_argument("--remote-debugging-port=9222")
-options.binary_location("/usr/bin/google-chrome")
+options.binary_location="/usr/bin/google-chrome"
 chrome_driver = webdriver.Remote("http://selenium:4444/wd/hub", options=options)
 chrome_driver.get('https://cs302-ui.vercel.app/')
 
