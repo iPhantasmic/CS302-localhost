544fa6e7aaaff66a980b9c447cecf2d9d6b3d02e
ONG CHI KIAT, NICHOLAS _ <nicholasong.2020@smu.edu.sg>
2022-11-04 18:40:59 +0800

    feat: new options arg

--- tests/selenium_get.py
+++ tests/selenium_get.py
@@ -5,12 +5,13 @@ from selenium.webdriver import ChromeOptions
 
 
 options = ChromeOptions()
+options.add_argument("--no-sandbox"); 
 options.add_argument("start-maximized"); 
 options.add_argument("disable-infobars");
 options.add_argument("--disable-extensions"); 
 options.add_argument("--disable-gpu");
 options.add_argument("--disable-dev-shm-usage");
-options.add_argument("--no-sandbox"); 
+options.add_argument("--remote-debugging-port=9222")
 chrome_driver = webdriver.Remote("http://selenium:4444/wd/hub", options=options)
 chrome_driver.get('https://cs302-ui.vercel.app/')
 
--- tests/selenium_test.py
+++ tests/selenium_test.py
@@ -4,6 +4,13 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 options = ChromeOptions()
+options.add_argument("--no-sandbox"); 
+options.add_argument("start-maximized"); 
+options.add_argument("disable-infobars");
+options.add_argument("--disable-extensions"); 
+options.add_argument("--disable-gpu");
+options.add_argument("--disable-dev-shm-usage");
+options.add_argument("--remote-debugging-port=9222")
 chrome_driver = webdriver.Remote("http://selenium:4444/wd/hub", options=options)
 chrome_driver.get('https://cs302-ui.vercel.app/')
 
