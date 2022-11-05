0d6132589e3efc0d8551bee68b3e6d6b1294a6e7
Justin <justin.100600@gmail.com>
2022-11-06 02:29:52 +0800

    feature: change to env vars

--- src/services/booking/BookingClient.ts
+++ src/services/booking/BookingClient.ts
@@ -1,19 +1,18 @@
-import fs from 'fs';
-import * as path from 'path';
 import * as grpc from '@grpc/grpc-js';
 import * as protoLoader from '@grpc/proto-loader';
 
-const packageDefinition:any = protoLoader.loadSync(path.resolve('./src/services/pb/bookings.proto'));
+const packageDefinition:any = protoLoader.loadSync('../src/services/pb/bookings.proto');
 
 const proto:any = grpc.loadPackageDefinition(packageDefinition).bookings;
 
-
-
 const credentials:any = grpc.credentials.createInsecure();
+
+const BOOKINGS_SVC_URL = process.env.BOOKINGS_SVC_URL
+console.log('The BOOKINGS_SVC_URL is:', BOOKINGS_SVC_URL);
 
 const options:any = {
 //   'grpc.ssl_target_name_override': 'localhost',
 //   interceptors: [interceptorAuth]
 };
 
-export default () => new proto.BookingService('localhost:50051', credentials);
+export default () => new proto.BookingService(BOOKINGS_SVC_URL, credentials);
--- src/services/review/ReviewClient.ts
+++ src/services/review/ReviewClient.ts
@@ -1,19 +1,18 @@
-import fs from 'fs';
-import * as path from 'path';
 import * as grpc from '@grpc/grpc-js';
 import * as protoLoader from '@grpc/proto-loader';
 
-const packageDefinition:any = protoLoader.loadSync(path.resolve('./src/services/pb/reviews.proto'));
+const packageDefinition:any = protoLoader.loadSync('../src/services/pb/reviews.proto');
 
 const proto:any = grpc.loadPackageDefinition(packageDefinition).reviews;
 
-
-
 const credentials:any = grpc.credentials.createInsecure();
+
+const REVIEWS_SVC_URL = process.env.REVIEWS_SVC_URL
+console.log('The REVIEWS_SVC_URL is:', REVIEWS_SVC_URL);
 
 const options:any = {
 //   'grpc.ssl_target_name_override': 'localhost',
 //   interceptors: [interceptorAuth]
 };
 
-export default () => new proto.ReviewService('localhost:50054', credentials);
+export default () => new proto.ReviewService(REVIEWS_SVC_URL, credentials);
