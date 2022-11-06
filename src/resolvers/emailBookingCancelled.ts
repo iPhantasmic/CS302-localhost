3f88e467ccded8a639d805fd1a83d0723cceecb5
ONG CHI KIAT, NICHOLAS _ <nicholasong.2020@smu.edu.sg>
2022-11-06 16:03:15 +0800

    chore: remove seed data

--- src/resolvers/emailBookingCancelled.ts
+++ src/resolvers/emailBookingCancelled.ts
@@ -5,20 +5,8 @@ const email_publisher = new SNSPublisher()
 export default (root: any, params: any) => {
     return new Promise((resolve: any, reject: any) => {
         try {
-            const test_data = {
-                data: {
-                    receiverEmail: 'nicholasongck@gmail.com',
-                    propertyName: 'Seminyak Kuta Utara',
-                    refundDate: 'November 7, 2022',
-                    unitPrice: 125.1,
-                    numNights: 5,
-                    subTotal: 625.5,
-                    serviceFee: 95.5,
-                    totalRefunded: 721.0,
-                },
-            }
             const response =
-                email_publisher.publish_message_booking_cancelled(test_data)
+                email_publisher.publish_message_booking_cancelled(params)
             resolve(response)
         } catch (err: any) {
             return reject(err)
