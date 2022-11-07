38381e3c34322a8a2f173fef3f59f373dff79187
Justin <justin.100600@gmail.com>
2022-11-07 12:32:37 +0800

    Update main.tf

--- cd/main.tf
+++ cd/main.tf
@@ -70,5 +70,6 @@ resource "aws_ecs_service" "auth-service" {
   name = "auth-service"
   cluster = "arn:aws:ecs:ap-southeast-1:631945473733:cluster/cs302-auth"
   task_definition = aws_ecs_task_definition.auth-service.arn
-  desired_count = 1
+  desired_count = 2
+  deployment_minimum_healthy_percent = 50
 }
