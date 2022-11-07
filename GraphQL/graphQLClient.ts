e3fb5881c07583100ad2bc138dd54957de5db53f
ONG CHI KIAT, NICHOLAS _ <nicholasong.2020@smu.edu.sg>
2022-11-07 12:33:45 +0800

    fix: http

--- GraphQL/graphQLClient.ts
+++ GraphQL/graphQLClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 // TODO: Swap on deploy
 const endpoint =
-  "cs302-api-gateway-4c723f2cbf13b8ea.elb.ap-southeast-1.amazonaws.com:6969";
+  "http://cs302-api-gateway-4c723f2cbf13b8ea.elb.ap-southeast-1.amazonaws.com:6969/";
 
 const gqlclient = new ApolloClient({
   uri: endpoint,
